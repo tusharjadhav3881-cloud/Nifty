99c48de72e6d2a82afebe8aac9839372fe9b206e
tusharjadhav3881-cloud <tushar.jadhav3881@gmail.com>
2025-09-26 22:20:50 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import seaborn as sb
+import matplotlib.pyplot as plt
+
+df=pd.read_csv("../Datasets/Nifty_Stocks.csv")
+
+df.Date=df.Date.astype('datetime64[ns]')
+
+print(df.Category.unique())
+c=input("Enter Category Name:")
+d=df[df.Category==c]
+
+print(df.Symbol.unique())
+s=input("Enter Symbol Name:")
+r=df[df.Symbol==s]
+
+sb.lineplot(x=r.Date,y=r.Close)
+plt.xticks(rotation=90)
+plt.show()
